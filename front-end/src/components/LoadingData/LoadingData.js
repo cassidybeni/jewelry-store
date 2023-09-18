8d7fc1f171794edf33e9884daf4ef807e18e296f
cassidybeni <cassidybeni@pursuit.org>
2023-09-17 23:45:03 -0400

    Loading data

--- front-end/src/components/LoadingData/LoadingData.js
+++ front-end/src/components/LoadingData/LoadingData.js
@@ -4,7 +4,7 @@ import "./LoadingData.css"
 function LoadingData() {
   return (
     <div className="data-container">
-      <h3 className="data-heading">Hang tight...</h3>
+      <h3 className="data-heading">Just a moment, please...</h3>
       <p>
         Data from Hell's Kitchen Ice is hosted on a free cloud web service,
         which may result in an initial response delay of up to 30 seconds.
