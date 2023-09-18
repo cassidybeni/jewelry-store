0434835049e2ac9927da4ccd0ed761a92b068741
cassidybeni <cassidybeni@pursuit.org>
2023-09-17 23:48:38 -0400

    Loading data

--- front-end/src/components/LoadingData/LoadingData.js
+++ front-end/src/components/LoadingData/LoadingData.js
@@ -5,10 +5,6 @@ function LoadingData() {
   return (
     <div className="data-container">
       <h3 className="data-heading">Just a moment, please...</h3>
-      <p>
-        Data from Hell's Kitchen Ice is hosted on a free cloud web service,
-        which may result in an initial response delay of up to 30 seconds.
-      </p>
     </div>
   );
 }
