d62f2ec243af0cd366eee8219b1dd24f93437f00
cassidybeni <cassidybeni@pursuit.org>
2023-09-21 20:58:45 -0400

    change cart image

--- front-end/src/components/NavBar/NavBar.js
+++ front-end/src/components/NavBar/NavBar.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./NavBar.css";
+import shoppingBag from "./cart.png"
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const goToCart = () => {
+    navigate("/cart");
+  };
+
   return (
     <div className="nav-container">
       <h1 className="main-header">HELL'S KITCHEN ICE</h1>
@@ -13,7 +21,7 @@ function NavBar() {
         <Link to="/necklaces">Necklaces</Link>
         <Link to="/rings">Rings</Link>
         <Link to="/watches">Watches</Link>
-        <Link to="/cart">Cart</Link>
+        <button onClick={goToCart} className="cart-button"><img src={shoppingBag} alt="Cart"></img></button>
       </nav>
     </div>
   );
