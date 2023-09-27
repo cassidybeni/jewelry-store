5a96cae2b1c7e389ed29dd65d08ad56be0a2e69b
cassidybeni <cassidybeni@pursuit.org>
2023-09-27 16:32:32 -0400

    Change loading page

--- front-end/src/components/LoadingData/LoadingData.js
+++ front-end/src/components/LoadingData/LoadingData.js
@@ -5,7 +5,7 @@ function LoadingData() {
   return (
     <div className="data-container">
       <h3 className="data-heading">Just a moment, please...</h3>
-      <p>HKI data is hosted on a free cloud web-service. This may cause a response delay of up to 30 seconds for the first request.</p>
+      <p>HKI data is stored on a free cloud service which may lead to a 30-second response delay for the first request.</p>
     </div>
   );
 }
