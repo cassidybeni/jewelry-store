df935d735414c764339ecd8b4473128a4e80973a
cassidybeni <cassidybeni@pursuit.org>
2023-09-27 16:35:36 -0400

    Loading

--- front-end/src/components/LoadingData/LoadingData.js
+++ front-end/src/components/LoadingData/LoadingData.js
@@ -4,7 +4,7 @@ import "./LoadingData.css"
 function LoadingData() {
   return (
     <div className="data-container">
-      <h3 className="data-heading">Just a moment, please...</h3>
+      <h3 className="data-heading">One moment, please...</h3>
       <p>HKI data is stored on a free cloud service which may lead to a 30-second response delay for the first request.</p>
     </div>
   );
