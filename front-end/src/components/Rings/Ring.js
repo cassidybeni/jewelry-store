d079acd7a4ed41bc8493236943b1d9d970c27b48
cassidybeni <cassidybeni@pursuit.org>
2023-10-03 18:36:25 -0400

    Fix cards

--- front-end/src/components/Rings/Ring.js
+++ front-end/src/components/Rings/Ring.js
@@ -2,19 +2,20 @@ import { Link } from "react-router-dom";
 
 function Ring({ ring }) {
   return (
-    <div className="column">
-      <Link to={`/rings/${ring.id}`} className="card-link">
+      <div className="column">
+      <Link to={`/rings/${ring.id}`}>
         <div className="card">
-          <img className="list-image" src={ring.image} alt={ring.name} />
+          <img src={ring.image} alt={ring.name} />
           <h4>
             <b className="product-name">{ring.name}</b>
           </h4>
           <button className="addToCart-btn-list">
+            {" "}
             <span>{ring.price}</span>
           </button>
         </div>
-      </Link>
-    </div>
+        </Link>
+      </div>
   );
 }
 
